06fae49d2a65f598ea18678c8143209abc359c73
uybli <82958252+uybli@users.noreply.github.com>
2022-02-01 11:40:05 +0100

    extracted extension point access; created custom item provider with extension point access

--- bundles/org.palladiosimulator.pcm.uncertainty.variation.UncertaintyVariationModel/src/UncertaintyVariationModel/statehandler/impl/ConcreteStateHandlerFactory.java
+++ bundles/org.palladiosimulator.pcm.uncertainty.variation.UncertaintyVariationModel/src/UncertaintyVariationModel/statehandler/impl/ConcreteStateHandlerFactory.java
@@ -1,12 +1,9 @@
 package UncertaintyVariationModel.statehandler.impl;
 
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IConfigurationElement;
-import org.eclipse.core.runtime.Platform;
-
 import UncertaintyVariationModel.VariationPoint;
 import UncertaintyVariationModel.statehandler.StateHandler;
 import UncertaintyVariationModel.statehandler.StateHandlerFactory;
+import UncertaintyVariationModel.statehandler.util.StateHandlerImplementation;
 
 public class ConcreteStateHandlerFactory implements StateHandlerFactory {
     public ConcreteStateHandlerFactory() {
@@ -19,25 +16,8 @@ public class ConcreteStateHandlerFactory implements StateHandlerFactory {
     @Override
     public StateHandler createFor(final VariationPoint object) {
         String stateHandlerId = object.getStateHandlerId();
-        IConfigurationElement[] config = Platform.getExtensionRegistry()
-            .getConfigurationElementsFor(STATEHANDLER_IMPLEMENTATION_ID);
-        try {
-            for (IConfigurationElement element : config) {
-
-                if (stateHandlerId.equalsIgnoreCase(element.getAttribute(ID_ATTRIBUTE))) {
-                    System.out.println("Extention with name: " + element.getAttribute(NAME_ATTRIBUTE));
-                    final StateHandler hnd = (StateHandler) element.createExecutableExtension(CLASS_ATTRIBUTE);
-                    return hnd;
-                }
-            }
-        } catch (CoreException ex) {
-            System.out.println(ex.getStackTrace());
-        }
-        throw new UnsupportedOperationException("Unknown stateHandlerId " + stateHandlerId);
+        StateHandlerImplementation impl = new StateHandlerImplementation();
+        return impl.getImplemationOf(stateHandlerId)
+            .orElseThrow(() -> new UnsupportedOperationException("Unknown stateHandlerId " + stateHandlerId));
     }
-
-    private static final String STATEHANDLER_IMPLEMENTATION_ID = "org.palladiosimulator.pcm.uncertainty.variation.UncertaintyVariationModel.statehandler_implementation";
-    private static final String CLASS_ATTRIBUTE = "class";
-    private static final String NAME_ATTRIBUTE = "name";
-    private static final String ID_ATTRIBUTE = "id";
 }
