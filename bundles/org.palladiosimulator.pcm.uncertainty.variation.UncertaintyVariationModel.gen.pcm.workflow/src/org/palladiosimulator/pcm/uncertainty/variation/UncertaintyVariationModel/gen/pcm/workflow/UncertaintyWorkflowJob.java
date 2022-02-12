427e14eb767ae60bf12b2ba096e0afcd1cb945bb
Maximilian Walter <maximilian.walter@kit.edu>
2022-02-12 16:13:39 +0100

    added workflow for creating new varations

---
+++ bundles/org.palladiosimulator.pcm.uncertainty.variation.UncertaintyVariationModel.gen.pcm.workflow/src/org/palladiosimulator/pcm/uncertainty/variation/UncertaintyVariationModel/gen/pcm/workflow/UncertaintyWorkflowJob.java
@@ -0,0 +1,29 @@
+package org.palladiosimulator.pcm.uncertainty.variation.UncertaintyVariationModel.gen.pcm.workflow;
+
+import java.util.Objects;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.emf.common.util.URI;
+import org.palladiosimulator.pcm.uncertainty.variation.UncertaintyVariationModel.gen.pcm.UncertaintyVariationModelGenPcm;
+
+import de.uka.ipd.sdq.workflow.jobs.JobFailedException;
+import de.uka.ipd.sdq.workflow.jobs.SequentialJob;
+import de.uka.ipd.sdq.workflow.jobs.UserCanceledException;
+
+public class UncertaintyWorkflowJob extends SequentialJob {
+
+	private URI uri;
+
+	public UncertaintyWorkflowJob(URI uri) {
+		Objects.nonNull(uri);
+		this.uri = uri;
+	}
+
+	@Override
+	public void execute(final IProgressMonitor monitor) throws JobFailedException, UserCanceledException {
+		var generation = new UncertaintyVariationModelGenPcm(uri.toString());
+		generation.generateVariations(monitor);
+
+	}
+
+}
