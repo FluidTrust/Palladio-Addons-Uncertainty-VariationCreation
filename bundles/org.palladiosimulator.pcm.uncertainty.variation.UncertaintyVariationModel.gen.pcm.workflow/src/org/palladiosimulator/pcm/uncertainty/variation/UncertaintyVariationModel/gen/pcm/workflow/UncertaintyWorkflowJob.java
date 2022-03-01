15ec093999f5b9acbba997681ecd4ce0144add35
Maximilian Walter <maximilian.walter@kit.edu>
2022-03-01 12:54:13 +0100

    workflow update

--- bundles/org.palladiosimulator.pcm.uncertainty.variation.UncertaintyVariationModel.gen.pcm.workflow/src/org/palladiosimulator/pcm/uncertainty/variation/UncertaintyVariationModel/gen/pcm/workflow/UncertaintyWorkflowJob.java
+++ bundles/org.palladiosimulator.pcm.uncertainty.variation.UncertaintyVariationModel.gen.pcm.workflow/src/org/palladiosimulator/pcm/uncertainty/variation/UncertaintyVariationModel/gen/pcm/workflow/UncertaintyWorkflowJob.java
@@ -23,9 +23,10 @@ public class UncertaintyWorkflowJob extends SequentialJob {
     @Override
     public void execute(final IProgressMonitor monitor) throws JobFailedException, UserCanceledException {
         try {
-            var generation = new UncertaintyVariationModelGenPcm(uri);
+            var generation = new UncertaintyVariationModelGenPcm(this.uri);
             generation.generateVariations(monitor);
         } catch (CoreException e) {
+            throw new JobFailedException(e);
         }
     }
 
